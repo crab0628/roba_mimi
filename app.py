de05cd63f61727d5333b8364e1ab4c5968691266
Akane Yamamoto <3lines.athome@gmail.com>
2020-08-19 20:33:44 +0900

    index,base,in作成

--- app.py
+++ app.py
@@ -4,6 +4,16 @@ from flask import Flask, render_template, request, session, redirect
 # appという名でアプリを作る宣言
 app = Flask(__name__)
 
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/in")
+def in_html():
+    return render_template("in.html")
+
+
+
 
 
 
