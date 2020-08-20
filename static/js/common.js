b24e26b1ea557a2f5b790ad0c88f48a4fe2019a7
Akane Yamamoto <3lines.athome@gmail.com>
2020-08-20 13:46:05 +0900

    js動作確認済み

---
+++ static/js/common.js
@@ -0,0 +1,23 @@
+// 自分以外もわかるよう、コメントはしつこいくらい入れましょう！
+// js効いてるか確認用
+// (function(){
+//     'use strict';
+
+//     alert('アラートだよ〜。');
+
+// })();
+
+// text img ボタン切り替え
+$(function() {
+    $('.switch_btn').click(function() {
+      $('.active').removeClass('active');
+      
+      // 変数clickedIndexを定義し、クリックしたボタンのインデックス番号を代入してください
+      var clickedIndex = $('.switch_btn').index($(this));
+      
+      // eqの引数をclickedIndexに書き換えてください
+      $('.form').eq(clickedIndex).addClass('active');
+    });
+  });
+  
+  
