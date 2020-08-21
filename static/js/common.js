d76f1e4c9a421e7ffe106f0119203339a002d6d1
Akane Yamamoto <3lines.athome@gmail.com>
2020-08-21 14:51:54 +0900

    さゆりさん分反映

--- static/js/common.js
+++ static/js/common.js
@@ -21,17 +21,83 @@ $(function() {
   });
   
 
-// ①試し
+// 試し①
+// $(function() {
+//   $('#file-sample').on('change', function(e) {
+//       // 1枚だけ表示する
+//       var file = e.target.files[0];
+
+//       // ファイルのブラウザ上でのURLを取得する
+//       var blobUrl = window.URL.createObjectURL(file);
+
+//       // img要素に表示
+//       $('#file-preview').attr('src', blobUrl);
+//   });
+// });  
+
+// 試し②
 $(function() {
-  $('#file-sample').on('change', function(e) {
-      // 1枚だけ表示する
-      var file = e.target.files[0];
+  $('input[type=file]').after('<span></span>');
 
-      // ファイルのブラウザ上でのURLを取得する
-      var blobUrl = window.URL.createObjectURL(file);
+  // アップロードするファイルを選択
+  $('input[type=file]').change(function() {
+    var file = $(this).prop('files')[0];
 
-      // img要素に表示
-      $('#file-preview').attr('src', blobUrl);
+    // 画像以外は処理を停止
+    if (! file.type.match('image.*')) {
+      // クリア
+      $(this).val('');
+      $('span').html('');
+      return;
+    }
+
+    // 新幅・高さ
+    var new_w = 200;
+    var new_h = 200;    
+
+    // 画像表示
+    var reader = new FileReader();
+    reader.onload = function() {
+      var img_src = $('<img>').attr('src', reader.result);
+
+      var org_img = new Image();
+      org_img.src = reader.result;
+      org_img.onload = function() {
+        // 元幅・高さ
+        var org_w = this.width;
+        var org_h = this.height;
+        // 幅 ＜ 規定幅 && 高さ ＜ 規定高さ
+        if (org_w < new_w && org_h < new_h) {
+          // 幅・高さは変更しない
+          new_w = org_w;
+          new_h = org_h;
+        } else {
+          // 幅 ＞ 規定幅 || 高さ ＞ 規定高さ
+          if (org_w > org_h) {
+            // 幅 ＞ 高さ
+            var percent_w = new_w / org_w;
+            // 幅を規定幅、高さを計算
+            new_h = Math.ceil(org_h * percent_w);
+          } else if (org_w < org_h) {
+            // 幅 ＜高さ
+            var percent_h = new_h / org_h;
+            // 高さを規定幅、幅を計算
+            new_w = Math.ceil(org_w * percent_h);
+          }
+        }
+
+        // リサイズ画像
+        $('span').html($('<canvas>').attr({
+          'id': 'canvas',
+          'width': new_w,
+          'height': new_h
+        }));
+        var ctx = $('#canvas')[0].getContext('2d');
+        var resize_img = new Image(); 
+        resize_img.src = reader.result;
+        ctx.drawImage(resize_img, 0, 0, new_w, new_h);
+      };
+    }
+    reader.readAsDataURL(file);
   });
-});  
-
+});
